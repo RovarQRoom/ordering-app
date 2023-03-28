4eead08132a07968c9a222712769087fb8fc5991
RovarQRoom <rovar.07000120@outlook.com>
2023-03-28 10:41:32 +0300

    Connected Our Mongo Database To Our Orders Module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -4,6 +4,9 @@ import { OrdersService } from './orders.service';
 import { ConfigModule } from '@nestjs/config';
 import Joi from 'joi';
 import { DatabaseModule } from '@app/common/database/database.module';
+import { OrdersRepository } from './repository/orders.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
   imports: [
@@ -15,8 +18,9 @@ import { DatabaseModule } from '@app/common/database/database.module';
       envFilePath: './apps/orders/.env',
     }), 
     DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
