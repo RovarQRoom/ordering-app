ea9d140d8ae0b73585c1d3a3645b4a0487453c64
RovarQRoom <rovar.07000120@outlook.com>
2023-03-28 13:22:05 +0300

    Added RabbitMQ To Orders.Service

--- apps/orders/src/service/orders.service.ts
+++ apps/orders/src/service/orders.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrdersDto, ReadOrdersDto } from '../Dto/orders.dto';
 import { OrdersRepository } from '../repository/orders.repository';
 import { IOrdersService } from '../interface/Iorders.service.interface';
+import { BILLING_SERVICE } from '../constants/services';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService implements IOrdersService {
-  constructor(private readonly ordersRepository:OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository:OrdersRepository, 
+    @Inject(BILLING_SERVICE) private readonly billingClient: ClientProxy
+    ) {}
 
   async createOrders(createOrdersDto: CreateOrdersDto): Promise<CreateOrdersDto> {
     await this.ordersRepository.create(createOrdersDto);
