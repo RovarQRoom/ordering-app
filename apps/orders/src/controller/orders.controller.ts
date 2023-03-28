575dffdc7d5dd613cf0234db40f503392cacc6a4
RovarQRoom <rovar.07000120@outlook.com>
2023-03-28 11:42:20 +0300

    Created Dto And Added Validation Pipes To main.ts

--- apps/orders/src/controller/orders.controller.ts
+++ apps/orders/src/controller/orders.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrdersService } from '../service/orders.service';
+import { CreateOrdersDto } from '../Dto/create-order.dto';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  @Post('create')
+  async createOrders(@Body() createOrdersDto: CreateOrdersDto ) {
+    return await this.ordersService.createOrders(createOrdersDto);
   }
 }
