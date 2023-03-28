dbd710f968a9bad6986045153e228406609327b2
RovarQRoom <rovar.07000120@outlook.com>
2023-03-28 13:02:31 +0300

    Made The Billing Read From The .env file

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { BillingController } from './controller/billing.controller';
 import { BillingService } from './service/billing.service';
 import { RmqModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [RmqModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+
+    }),
+    RmqModule
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
